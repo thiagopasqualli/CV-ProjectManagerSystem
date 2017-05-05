d5046a044ab5ad580cf458b2ab966ef892ebd91d
GabrielCarpes <gabrcarpes@gmail.com>
2017-05-05 16:01:42 -0300

    layout arrundo
    layout arrundo

--- ProjectManagerSystem/ProjectManagerSystem/Models/Projeto.cs
+++ ProjectManagerSystem/ProjectManagerSystem/Models/Projeto.cs
@@ -8,8 +8,8 @@ namespace ProjectManagerSystem.EntityFramework
     public class Projeto
     {
         public int Id { get; set; }
-        public int IdBR { get; set; }
-        public int IdLinx { get; set; }
+        public string IdBR { get; set; }
+        public string IdLinx { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public DateTime InicioPrevisto { get; set; }
