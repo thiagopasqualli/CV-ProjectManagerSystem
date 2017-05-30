4ba1855c267c6c1016e6281067d80f53953d03b4
thiagopasqualli <thiagopasqualli@gmail.com>
2017-05-30 11:30:00 -0300

    Adaptação do código para diretórios no IIS
    Alterado diretorios no ng-include, get/post dos scripts. Alterado webconfig para o banco de produção.

---
+++ myApp.js
@@ -0,0 +1,141 @@
+﻿var myApp = angular.module('myApp', []);
+myApp.controller('projetosCtrl', function ($http, $window) {
+    var ctrl = this;
+    ctrl.newData = null;
+    ctrl.btnAdd = true;
+    ctrl.btnSave = false;
+    ctrl.Classificacao = '';
+    ctrl.salvar = false;
+    ctrl.alterar = false;
+    ctrl.showAlterarButton = true;
+
+    // USUARIOS
+    //==========================================================================================
+
+    //GetUsuarios
+    $http.get("/pms_producao/PMS/GetUsuarios")
+      .then(function (response) {
+          ctrl.usuarios = response.data;
+      })
+      .catch(function (response) {
+          console.log(response);
+      });
+
+    //AddUsuarios
+    ctrl.AddUser = function () {
+        $http.post("/pms_producao/PMS/AddUsuarios", ctrl.newData);
+        location.reload();
+    }
+
+    //DeleteUsuario
+    ctrl.DeleteUser = function () {
+        $http.post("/pms_producao/PMS/DelUsuarios", ctrl.newData);
+        location.reload();
+    }
+
+    //Limpar
+    ctrl.Clear = function () {
+        ctrl.newData = {};
+    }
+
+
+    //Alterar
+    ctrl.AlterUser = function () {
+        $http.post("/pms_producao/PMS/AlterUsuarios", ctrl.newData);
+        ctrl.btnSave = false;
+        ctrl.btnAdd = true;
+        ctrl.salvar = false;
+        location.reload();
+    }
+
+    // PROJETOS
+    //==========================================================================================
+    //GetProjetos
+    $http.get("/pms_producao/PMS/GetProjetos")
+      .then(function (response) {
+          ctrl.projetos = response.data;
+      })
+
+      .catch(function (response) {
+          console.log(response);
+      });
+
+    //AddProjetos
+    ctrl.AddProjetos = function () {
+        $http.post("/pms_producao/PMS/AddProjetos", ctrl.newData);
+        location.reload();
+    }
+
+    //DeleteProjetos
+
+    ctrl.DelProjetos = function () {
+        $http.post("/pms_producao/PMS/DelProjetos", ctrl.newData);
+        location.reload();
+    }
+
+    //Alterar
+    ctrl.AlterProjetos = function () {
+        $http.post("/pms_producao/PMS/AlterProjetos", ctrl.newData);
+        ctrl.btnSave = false;
+        ctrl.btnAdd = true;
+        ctrl.salvar = false;
+        location.reload();
+    }
+
+    // OCORRÊNCIAS
+    //==========================================================================================
+
+    //GetOcorrencias
+    ctrl.GetOcorrencias = function () {
+        $http.post("/pms_producao/PMS/GetListaOcorrencias/" + ctrl.newData.Id)
+          .then(function (response) {
+              ctrl.ocurrences = response.data;
+          })
+          .catch(function (response) {
+              console.log(response);
+          });
+    }
+
+    //AddOcorrencias
+    ctrl.AddOcorrencia = function () {
+        $http.post("/pms_producao/PMS/AddOcorrencias/" + ctrl.newData.Id, { Desc: ctrl.newData.Desc, Titulo: ctrl.newData.Titulo });
+        location.reload();
+    }
+
+    ctrl.HabilitarEdicao = function () {
+        ctrl.alterar = false;
+        ctrl.showAlterarButton = false;
+        ctrl.salvar = true;
+    }
+
+    ctrl.Reload = function () {
+
+        ctrl.newData = {};
+        ctrl.btnAdd = true;
+        ctrl.btnSave = false;
+        ctrl.salvar = false;
+        ctrl.alterar = false;
+        ctrl.showAlterarButton = true;
+    }
+
+    ctrl.ExcluirUser = function () {
+        $http.post("/pms_producao/PMS/DelUsuarios", ctrl.newData).then(function (response) {
+            if ((response.data) == "True")
+                window.alert("excluiu");
+            else
+                window.alert("Usuario Vinculado!");
+        })
+    }
+
+    //Selecionar 
+    ctrl.SelectUser = function (user) {
+        ctrl.newData = user;
+        ctrl.btnSave = true;
+        ctrl.btnAdd = false;
+        ctrl.showAlterarButton = true;
+        ctrl.visualizar = true;
+        ctrl.alterar = true;
+        ctrl.salvar = false;
+    }
+
+});
---
+++ userApp.js
@@ -0,0 +1,94 @@
+﻿var myApp = angular.module('myApp', []);
+myApp.controller('userCtrl', function ($http, $window) {
+    var ctrl = this;
+    ctrl.newData = null;
+    ctrl.btnAdd = true;
+    ctrl.salvar = false;
+    ctrl.alterar = false;
+    ctrl.showAlterarButton = true;
+    ctrl.showUserButtons = true;
+
+
+    //Get
+    $http.get("/pms_producao/PMS/GetUsuarios")
+      .then(function (response) {
+          ctrl.usuarios = response.data;
+      })
+      .catch(function (response) {
+          console.log(response);
+      });
+
+    //Add
+    ctrl.AddUser = function () {
+        $http.post("/pms_producao/PMS/AddUsuarios", ctrl.newData);
+        location.reload();
+    }
+
+    //Limpar
+    ctrl.Clear = function () {
+        ctrl.newData = {};
+        ctrl.verificaDadosLimpar() = true;
+    }
+
+    //Alterar
+    ctrl.AlterUser = function () {
+        $http.post("/pms_producao/PMS/AlterUsuarios", ctrl.newData);
+        ctrl.btnAdd = true;
+        ctrl.salvar = false;
+        location.reload();
+    }
+
+    ctrl.HabilitarEdicao = function () {
+        ctrl.alterar = false;
+        ctrl.showAlterarButton = false;
+        ctrl.salvar = true;
+        ctrl.showUserButtons = true;      
+
+    }
+
+    ctrl.Reload = function () {
+        ctrl.newData = {};
+        ctrl.btnAdd = true;
+        ctrl.btnSave = false;
+        ctrl.salvar = false;
+        ctrl.alterar = false;
+        ctrl.showAlterarButton = true;
+        ctrl.showUserButtons = true;
+        ctrl.verificaDadosLimpar() = true;
+
+        ctrl.newData.$setPristine();
+    }
+
+    ctrl.DelUser = function () {
+        confirma = confirm("Tem certeza que deseja excluir esse usuário?");
+        if (confirma) {
+            $http.post("/pms_producao/PMS/DelUsuarios", ctrl.newData).then(function (response) {
+                if ((response.data) == "True") {
+                    window.alert("Usuário excluído");
+                    location.reload();
+                }
+                else
+                    window.alert("Usuario vinculado a um projeto. Não é possível deletar.");
+            })
+        }
+    }
+
+    //Selecionar 
+    ctrl.SelectUser = function (user) {
+        ctrl.newData = user;
+        ctrl.btnSave = true;
+        ctrl.btnAdd = false;
+        ctrl.showAlterarButton = true;
+        ctrl.visualizar = true;
+        ctrl.alterar = true;
+        ctrl.salvar = false;
+        ctrl.showUserButtons
+    }
+
+    ctrl.verificaDadosLimpar = function () {
+        if (ctrl.newData == null)
+            return true;
+        else
+            return false;
+    }
+});
